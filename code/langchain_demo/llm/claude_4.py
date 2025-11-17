c64a6c4d6383690552fda399bdc17522f69ca066
“Marvelous-Tan” <tanlucheng888@gmail.com>
2025-11-17 17:52:01 +0800

    feats: add sth

--- code/langchain_demo/llm/claude_4.py
+++ code/langchain_demo/llm/claude_4.py
@@ -2,7 +2,7 @@ from langchain_anthropic import ChatAnthropic
 from env_utils import CLAUDE_API_KEY, CLAUSE_URL  # 注意拼写保持一致
 
 llm = ChatAnthropic(
-    model="claude-3-5-sonnet-20241022",  # Claude 最新模型名
+    model="claude-sonnet-4-5",  # Claude 最新模型名
     temperature=0.8,
     api_key=CLAUDE_API_KEY,
     base_url=CLAUSE_URL,                 # 你的代理或 Anthropic 官方接口
